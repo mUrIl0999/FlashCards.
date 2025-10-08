48943bc69617f099d33646f3281825fa7fa853c6
patolino <murilo.gustavo.oliveira@escola.pr.gov.br>
2025-10-08 01:12:58 +0000

    009

--- perguntas.js
+++ perguntas.js
@@ -1,10 +1,4 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
-)
-
-criaCartao(
     'Geografia',
     'Qual a capital da França?',
     'A capital da França é Paris'
@@ -20,4 +14,70 @@ criaCartao(
     'Lingua inglesa',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual a fórmula para calcular a área de um triângulo?',
+    'A = (b x h)/2'
+)
+
+criaCartao(
+    'História',
+    'Quem foi os principais participantes da guerra fria?',
+    'foram os Estados Unidos e a União Soviética'
+)
+
+criaCartao(
+    'Sociologia',
+    'Quais são as 3 áreas da Sociologia?',
+    'Sociologia, a Antropologia e a Ciência Política'
+)
+
+criaCartao(
+    'português',
+    'o que é uma oração',
+    'todo enunciado que se organiza em torno de um verbo ou locução verbal, que é o seu núcleo'
+)
+
+criaCartao(
+    'Educação financeira',
+    'oque é o metoo 50 30 20',
+    'O método 50/30/20 50% para Necessidades (ou Despesas Essenciais),30% para Desejos (ou Despesas Pessoais),20% para Prioridades Financeiras'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é Biologia?',
+    'A Biologia é a ciência que estuda a vida em todas as suas formas, desde o nível molecular até o planeta, focando em como os organismos se estruturam, funcionam, evoluem e interagem com o ambiente'
+)
+
+criaCartao(
+    'Física',
+    'Quais são as 5 áreas da física?',
+    'Mecânica, Termologia, Eletromagnetismo, Óptica e Ondulatória.'
+)
+
+criaCartao(
+    'biologia I ',
+    'O que se estuda em biologia 1?',
+    'engloba desde organismos microscópicos até os maiores seres vivos do planeta, analisando-os nos diferentes níveis'
+)
+
+criaCartao(
+    'Química',
+    'O que estuda Química?',
+    'a matéria – sua composição, estrutura, propriedades e as transformações que ela sofre, além das interações entre as substâncias e a energia envolvida nessas mudanças'
+)
+
+criaCartao(
+    'projeto de vida',
+    'Quais são os quatro elementos do projeto de vida?',
+    'o Autoconhecimento, a Visão de Futuro, as Ações para alcançar os objetivos e a A avaliação e reflexão contínuas sobre o processo.'
+)
+
+criaCartao(
+    'matemática I',
+    'O que se estuda em Matemática 1?',
+    'Estuda as noções de conjuntos e de funções polinomial, modular, racional, exponencial e logarítmica.'
 )
